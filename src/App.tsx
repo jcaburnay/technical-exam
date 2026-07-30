bd8d48c7bc0f5635b3eddab9028a6e95d8ee50b6
Jonathan Caburnay <jonathan@caburnay.dev>
2026-07-30 16:52:56 +0800

    feat: implement dog listing

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dogs, setDogs] = useState([
+    { name: "Otis", status: "listed" },
+    { name: "Sir Barks-a-lot", status: "listed" },
+    { name: "Scooby", status: "examining" },
+    { name: "Brownie", status: "finally_back_to_hooman" },
+  ]);
+  const [newDog, setNewDog] = useState("");
+  const listed = dogs.filter((dog) => dog.status === "listed");
+  const examining = dogs.filter((dog) => dog.status === "examining");
+  const finally_back_to_hooman = dogs.filter((dog) => dog.status === "finally_back_to_hooman");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDogs((prev) => [...prev, { name: newDog, status: "listed" }]);
+    setNewDog("");
+  };
+
+  const handleNext = (currentDog) => {
+    console.log(currentDog);
+    if (currentDog.status === "listed") {
+      const foundDog = dogs.find((dog) => dog.name === currentDog.name);
+      setDogs((prev) => [
+        ...prev.filter((dog) => dog.name !== currentDog.name),
+        { ...foundDog, status: "examining" },
+      ]);
+    } else if (currentDog.status === "examining") {
+      const foundDog = dogs.find((dog) => dog.name === currentDog.name);
+      setDogs((prev) => [
+        ...prev.filter((dog) => dog.name !== currentDog.name),
+        { ...foundDog, status: "finally_back_to_hooman" },
+      ]);
+    }
+  };
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <header className="flex-items justify">
+        <h1>Pet Manager 3000</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="dog-name">Dog name: </label>
+          <input name="dog-name" value={newDog} onChange={(e) => setNewDog(e.target.value)} />
+          <button type="submit">Add pet</button>
+        </form>
+      </header>
+      <main className="grid">
+        <div>
+          <h2>Listed</h2>
+          {listed.map((dog) => (
+            <div key={`${dog.name}-${dog.status}`} className="flex-items">
+              <li>{dog.name}</li>
+              <button type="button" onClick={() => handleNext(dog)}>
+                Next
+              </button>
+            </div>
+          ))}
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          <h2>Examining</h2>
+          {examining.map((dog) => (
+            <div key={`${dog.name}-${dog.status}`} className="flex-items">
+              <li key={`${dog.name}-${dog.status}`}>{dog.name}</li>
+              <button type="button" onClick={() => handleNext(dog)}>
+                Next
+              </button>
+            </div>
+          ))}
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+        <div>
+          <h2>Finally Back to Hooman</h2>
+          {finally_back_to_hooman.map((dog) => (
+            <div key={`${dog.name}-${dog.status}`} className="flex-items">
+              <li key={`${dog.name}-${dog.status}`}>{dog.name}</li>
+            </div>
+          ))}
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+// function ListOfDogs({ dogs }) {
+
+//   if (status === "listed") {
+//     return <div>
+//       <h2>Listed</h2>
+//       {listed.map((dog) => (
+//         <div key={`${dog.name}-${dog.status}`} className="flex-items">
+//           <li>{dog.name}</li>
+//           <button type="button" onClick={() => handleNext(dog)}>
+//             Next
+//           </button>
+//         </div>
+//       ))}
+//     </div>
+//   }
+// }
+
+export default App;
